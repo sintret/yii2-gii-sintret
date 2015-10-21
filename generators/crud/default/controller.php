d39ea55e1b3d1e0e995a51215d20ca04db14da95
andy <sintret@gmail.com>
2015-10-21 23:32:22 +0700

    first

--- generators/crud/default/controller.php
+++ generators/crud/default/controller.php
@@ -2,9 +2,10 @@
 /**
  * This is the template for generating a CRUD controller class file.
  */
-
+use yii\helpers\Inflector;
 use yii\db\ActiveRecordInterface;
 use yii\helpers\StringHelper;
+use yii\helpers\Url;
 
 
 /* @var $this yii\web\View */
@@ -36,18 +37,67 @@ use <?= ltrim($generator->searchModelClass, '\\') . (isset($searchModelAlias) ? 
 <?php else: ?>
 use yii\data\ActiveDataProvider;
 <?php endif; ?>
-use <?= ltrim($generator->baseControllerClass, '\\') ?>;
+<?php
+$num=0;
+foreach ($generator->getColumnNames() as $attribute) {
+   $attr[]=$attribute; 
+   if($attribute == 'image'){
+       $num=1;
+   }
+}
+if($num){
+    $loadfile = 'loadWithFiles';
+} else {
+    $loadfile = 'load';
+}
+?>
+
+use yii\helpers\Url;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+use sintret\gii\models\LogUpload;
+use sintret\gii\components\Util;
+
 
 /**
  * <?= $controllerClass ?> implements the CRUD actions for <?= $modelClass ?> model.
  */
-class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->baseControllerClass) . "\n" ?>
+class <?= $controllerClass ?> extends CController <?= "\n" ?>
 {
+
     public function behaviors()
     {
         return [
+        'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'rules' => [
+               //                    [
+//                        'allow' => true,
+//                        'actions' => ['index','view','sample','parsing-log','excel'],
+//                        'roles' => ['viewer']
+//                    ],
+//                    [
+//                        'allow' => true,
+//                        'actions' => ['create','parsing'],
+//                        'roles' => ['author']
+//                    ],
+//                    [
+//                        'allow' => true,
+//                        'actions' => ['update'],
+//                        'roles' => ['editor']
+//                    ],
+//                    [
+//                        'allow' => true,
+//                        'actions' => ['delete', 'delete-all'],
+//                        'roles' => ['admin']
+//                    ],
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -63,6 +113,23 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
      */
     public function actionIndex()
     {
+        $grid = 'grid-'.self::className();
+        $reset = Yii::$app->getRequest()->getQueryParam('p_reset');
+        if ($reset) {
+            \Yii::$app->session->set($grid, "");
+        } else {
+            $rememberUrl = Yii::$app->session->get($grid);
+            $current = Url::current();
+            if ($rememberUrl != $current && $rememberUrl) {
+                Yii::$app->session->set($grid, "");
+                $this->redirect($rememberUrl);
+            }
+            if (Yii::$app->getRequest()->getQueryParam('_pjax')) {
+                \Yii::$app->session->set($grid, "");
+                \Yii::$app->session->set($grid, Url::current());
+            }
+        }
+        
 <?php if (!empty($generator->searchModelClass)): ?>
         $searchModel = new <?= isset($searchModelAlias) ? $searchModelAlias : $searchModelClass ?>();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
@@ -103,8 +170,9 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
     {
         $model = new <?= $modelClass ?>();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', <?= $urlParams ?>]);
+        if ($model-><?=$loadfile;?>(Yii::$app->request->post()) && $model->save()) {
+            Yii::$app->session->setFlash('success', 'Well done! successfully to save data!  ');
+            return $this->redirect(['index']);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -122,8 +190,9 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
     {
         $model = $this->findModel(<?= $actionParams ?>);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', <?= $urlParams ?>]);
+        if ($model-><?=$loadfile;?>(Yii::$app->request->post()) && $model->save()) {
+            Yii::$app->session->setFlash('success', 'Well done! successfully to update data!  ');
+            return $this->redirect(['index']);
         } else {
             return $this->render('update', [
                 'model' => $model,
@@ -140,6 +209,7 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
     public function actionDelete(<?= $actionParams ?>)
     {
         $this->findModel(<?= $actionParams ?>)->delete();
+        Yii::$app->session->setFlash('success', 'Well done! successfully to deleted data!  ');
 
         return $this->redirect(['index']);
     }
@@ -169,5 +239,171 @@ if (count($pks) === 1) {
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
+    }
+    
+    public function actionSample() {
+
+        //$objPHPExcel = new \PHPExcel();
+        $template = Util::templateExcel();
+        $model = new <?= $modelClass ?>;
+        $date = date('YmdHis');
+        $name = $date.'<?= $modelClass ?>';
+        //$attributes = $model->attributeLabels();
+        $models = <?= $modelClass ?>::find()->all();
+        $excelChar = Util::excelChar();
+        $not = Util::excelNot();
+        
+        foreach ($model->attributeLabels() as $k=>$v){
+            if(!in_array($k, $not)){
+                $attributes[$k]=$v;
+            }
+        }
+
+        $objReader = \PHPExcel_IOFactory::createReader('Excel5');
+        $objPHPExcel = $objReader->load(Yii::getAlias($template));
+
+        return $this->render('sample', ['models' => $models,'attributes'=>$attributes,'excelChar'=>$excelChar,'not'=>$not,'name'=>$name,'objPHPExcel' => $objPHPExcel]);
+    }
+    
+    public function actionParsing() {
+        $num = 0;
+        $fields = [];
+        $values = [];
+        $log = '';
+        $route = '';
+        $model = new LogUpload;
+        
+        $date = date('Ymdhis') . Yii::$app->user->identity->id;
+
+        if (Yii::$app->request->isPost) {
+            $model->fileori = UploadedFile::getInstance($model, 'fileori');
+
+            if ($model->validate()) {
+                $fileOri = Yii::getAlias(LogUpload::$imagePath) . $model->fileori->baseName . '.' . $model->fileori->extension;
+                $filename = Yii::getAlias(LogUpload::$imagePath) . $date . '.' . $model->fileori->extension;
+                $model->fileori->saveAs($filename);
+            }
+            $params = Util::excelParsing(Yii::getAlias($filename));
+            $model->params = \yii\helpers\Json::encode($params);
+            $model->title = 'parsing <?= $modelClass ?>';
+            $model->fileori = $fileOri;
+            $model->filename = $filename;
+
+
+            if ($params)
+                foreach ($params as $k => $v) {
+                    foreach ($v as $key => $val) {
+                        if ($num == 0) {
+                            $fields[$key] = $val;
+                            $max = $key;
+                        }
+
+                        if ($num >= 3) {
+                            $values[$num][$fields[$key]] = $val;
+                        }
+                    }
+                    $num++;
+                }
+            if (in_array('id', $fields)) {
+                $model->type = LogUpload::TYPE_UPDATE;
+            } else {
+                $model->type = LogUpload::TYPE_INSERT;
+            }
+            $model->keys = \yii\helpers\Json::encode($fields);
+            $model->values = \yii\helpers\Json::encode($values);
+            if ($model->save()) {
+                $log = 'log_<?= $modelClass ?>'. Yii::$app->user->id;
+                Yii::$app->session->setFlash('success', 'Well done! successfully to Parsing data, see log on log upload menu! Please Waiting for processing indicator if available...  ');
+                Yii::$app->session->set($log, $model->id);
+                $notification = new \sintret\gii\models\Notification;
+                $notification->title = 'parsing <?= $modelClass ?>';
+                $notification->message = Yii::$app->user->identity->username . ' parsing <?= $modelClass ?> ';
+                $notification->params = \yii\helpers\Json::encode(['model' => '<?= $modelClass ?>', 'id' => $model->id]);
+                $notification->save();
+            }
+        }
+        $route = '<?= strtolower($modelClass) ?>/parsing-log';
+
+        return $this->render('parsing', ['model' => $model,'log'=>$log,'route'=>$route]);
+    }
+    
+    public function actionParsingLog($id) {
+        $mod = LogUpload::findOne($id);
+        $type = $mod->type;
+        $keys = \yii\helpers\Json::decode($mod->keys);
+        $values = \yii\helpers\Json::decode($mod->values);
+        $modelAttribute = new <?= $modelClass ?>;
+        $not = Util::excelNot();
+        
+            foreach ($values as $value) {
+                if ($type == LogUpload::TYPE_INSERT)
+                    $model = new <?= $modelClass ?>;
+                else
+                    $model = <?= $modelClass ?>::findOne($value['id']);
+
+                foreach ($keys as $v) {
+                        $model->$v = $value[$v];
+                }
+                
+                $e = 0;
+                if ($model->save()) {
+                    $model = NULL;
+                    $pos = NULL;
+                } else {
+                    $error[] = \yii\helpers\Json::encode($model->getErrors());
+                    $e = 1;
+                }
+            }
+
+        if ($error) {
+            foreach ($error as $err) {
+                if ($err) {
+                    $er[] = $err;
+                    $e+=1;
+                }
+            }
+            if ($e) {
+                $mod->warning = \yii\helpers\Json::encode($er);
+                $mod->save();
+                echo '<pre>';
+                print_r($er);
+            }
+        }
+    }
+
+    public function actionExcel() {
+        $searchModel = new <?= $modelClass ?>Search;
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        $modelAttribute = new <?= $modelClass ?>;
+        $not = Util::excelNot();
+        foreach ($modelAttribute->attributeLabels() as $k=>$v){
+            if(!in_array($k, $not)){
+                $attributes[$k] = $v;
+            }
+        }
+
+        $models = $dataProvider->getModels();
+        $objReader = \PHPExcel_IOFactory::createReader('Excel5');
+        $objPHPExcel = $objReader->load(Yii::getAlias(Util::templateExcel()));
+        $excelChar = Util::excelChar();
+        return $this->render('_excel', [
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
+                    'attributes' => $attributes,
+                    'models' => $models,
+                    'objReader' => $objReader,
+                    'objPHPExcel' => $objPHPExcel,
+                    'excelChar' => $excelChar
+        ]);
+    }
+    public function actionDeleteAll() {
+        $pk = Yii::$app->request->post('pk'); // Array or selected records primary keys
+        $explode = explode(",", $pk);
+        if ($explode)
+            foreach ($explode as $v) {
+                if($v)
+                $this->findModel($v)->delete();
+            }
+        echo 1;
     }
 }
